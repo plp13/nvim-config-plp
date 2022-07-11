f82ffd784afd398c25b73b27b6642aa31aae5447
Pantelis Panayiotou <info@miliaris.eu>
2022-07-11 11:01:53 +0300

    Fixed bug that enabled Rust-related LSP keymaps for non-Rust buffers

--- lua/user/rust-tools.lua
+++ lua/user/rust-tools.lua
@@ -1,9 +1,31 @@
 -- Custom options for rust-tools.nvim: advanced LSP client for Rust
+-- Note: overrides NeoVim's default LSP client, and needs to be configured separately
 
 -- Require rust-tools (exit if it's not installed)
 local status_ok, rust_tools = pcall(require, "rust-tools")
 if not status_ok then
   return
+end
+
+-- Helper of on_attach(): configure additional which-key mappings for rust-tools.nvim
+local function lsp_keymaps(bufnr)
+  local wk_status_ok, wk = pcall(require, "which-key")
+  if wk_status_ok then
+    wk.register({
+      ["<Leader>lr"] = { "<cmd>RustRun<cr>", "Run program" },
+    }, { mode = "n", buffer = bufnr })
+  end
+end
+
+-- Function that is called whenever a buffer with a Rust LSP server is attached
+local function on_attach(client, bufnr)
+  -- Call the on_attach() function in our standard LSP configuration
+  local lsp_status_ok, lsp = pcall(require, "user.lsp.handlers")
+  if lsp_status_ok then
+    lsp.on_attach(client, bufnr)
+  end
+  -- Call lsp_keymaps()
+  lsp_keymaps(bufnr)
 end
 
 rust_tools.setup {
@@ -16,17 +38,11 @@ rust_tools.setup {
       highlight = "MoonflyGrey236",
     },
   },
+  -- LSP server options: similar to the ones in our standard LSP configuration
   server = {
-    -- The following two lines are required because rust-tools overrides LSP's default configuration
-    on_attach = require("user.lsp.handlers").on_attach,
+    -- Use the locally defined on_attach()
+    on_attach = on_attach,
+    -- Use the server capabilities defined in the standard configuration
     capabilities = require("user.lsp.handlers").capabilities,
   },
 }
-
--- Which-key mappings for rust-tools.nvim
-local wk_status_ok, wk = pcall(require, "which-key")
-if wk_status_ok then
-  wk.register({
-    ["<Leader>lr"] = { "<cmd>RustRun<cr>", "Run program" },
-  }, { mode = "n" })
-end
