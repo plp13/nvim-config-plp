05a75dad9c1a84f07d98e3352d4d81b567192e47
Pantelis Panayiotou <info@miliaris.eu>
2022-07-12 17:01:07 +0300

    Minor bug fixes for popui

--- lua/user/popui.lua
+++ lua/user/popui.lua
@@ -1,14 +1,14 @@
 -- Configuration for popui.nvim: a popup window for vim.ui.select and vim.ui.input actions
 
 -- Require popui (exit if it's not installed)
--- local status_ok, _ = pcall(require, "popui.ui-overrider")
--- if not status_ok then
---   return
--- end
+local status_ok, _ = pcall(require, "popui.ui-overrider")
+if not status_ok then
+  return
+end
 
 -- Override vim.ui.select and vim.ui.input
 vim.ui.select = require("popui.ui-overrider")
 vim.ui.input = require("popui.input-overrider")
 
 -- Use rounded border
-vim.g.popui_border_style = "double"
+vim.g.popui_border_style = "rounded"
