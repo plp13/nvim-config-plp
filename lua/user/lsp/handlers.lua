371c5d0dfcb4e9b1ba31cc1a3537fa16eac43cac
Pantelis Panayiotou <p.panayiotou@gmail.com>
2025-10-01 16:06:53 +0300

    Remove call to deprecated vim.fn.sign_define()

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -6,23 +6,17 @@ local M = {}
 
 -- LSP server set up function
 M.setup = function()
-  -- Diagnostic signs (ie icons)
-  local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-  }
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
-
   local config = {
     -- Disable virtual text
     virtual_text = false,
-    -- Enable diagnostic signs, as defined above
+    -- Custom diagnostic signs
     signs = {
-      active = signs,
+      text = {
+        [vim.diagnostic.severity.ERROR] = "",
+        [vim.diagnostic.severity.WARN] = "",
+        [vim.diagnostic.severity.HINT] = "",
+        [vim.diagnostic.severity.INFO] = ""
+      }
     },
     -- Update diagnostics as you type
     update_in_insert = true,
